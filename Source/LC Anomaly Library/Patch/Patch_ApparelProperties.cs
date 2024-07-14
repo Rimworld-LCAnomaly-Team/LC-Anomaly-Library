6d64346f946ea7a2e08ac024c34b017bfcd2c22c
DarthCY <452710557@qq.com>
2024-07-14 23:26:26 +0800

    add：提供口部渲染层支持

---
+++ Source/LC Anomaly Library/Patch/Patch_ApparelProperties.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LCAnomalyLibrary.Patch
+{
+    /// <summary>
+    /// 服装层Patch（用于解决头部服装渲染问题）
+    /// </summary>
+    [HarmonyPatch(typeof(ApparelProperties), nameof(ApparelProperties.LastLayer), MethodType.Getter)]
+    public class Patch_ApparelProperties
+    {
+        private static bool Prefix(ApparelProperties __instance, ref ApparelLayerDef __result)
+        {
+            //如果是自定义的头部层就截断原方法，强制返回为原版的头部层
+            var layers = __instance.layers;
+            if (layers.Count > 0)
+            {
+                var tmpLayer = layers[layers.Count - 1];
+                if(tmpLayer == Defs.ApparelLayerDefOf.LC_AccessoryMouth)
+                {
+                    __result = ApparelLayerDefOf.Overhead;
+                    return false;
+                }
+            }
+
+            //如果不是就执行原方法
+            return true;
+        }
+    }
+}
